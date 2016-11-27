5a266e6fb8b012524d9cd4826342c965a05d8917
Balint Pato <balint.pato@balopat.com>
2016-11-26 23:20:06 -0500

    drill 1.2.1 - division, multiplication, substraction operators

--- haskell/drill_1_1_1.hs
+++ haskell/drill_1_1_1.hs
@@ -10,6 +10,16 @@ readComplex s = let parts = splitOn " " s
 (<+>) :: Complex -> Complex -> Complex
 (Complex r1 i1) <+>  (Complex r2 i2) = Complex (r1 + r2) (i1 + i2)
 
+(<->) :: Complex -> Complex -> Complex
+(Complex r1 i1) <->  (Complex r2 i2) = Complex (r1 - r2) (i1 - i2)
+
+(|*|) :: Complex -> Complex -> Complex
+(Complex r1 i1) |*| (Complex r2 i2) = Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
+
+(</>) :: Complex -> Complex -> Complex
+(Complex r1 i1) </>  (Complex r2 i2) = Complex ((r1 * r2 + i1 * i2)/(r2*r2 + i2 * i2)) ((r2 * i1 - r1 * i2)/(r2*r2 + i2 * i2))
+
+
 main = do
   print "Specify c1 (complex number) in form a b"   
   c1String <- getLine
@@ -17,5 +27,15 @@ main = do
   print "Specify c2 (complex number) in form a b"   
   c2String <- getLine
   
-  let sum = readComplex c1String <+> readComplex c2String
-  print sum
+  let c1 = readComplex c1String
+  
+  let c2 = readComplex c2String 
+  
+  print $ "c1 + c2: " ++ show(c1 <+> c2)   
+  print $ "c1 - c2 : " ++ show(c1 <-> c2) 
+  print $ "c2 - c1: " ++ show(c2 <-> c1) 
+  print $ "c1 * c2: " ++ show(c1 |*| c2) 
+  print $ "c2 * c1 / c1: " ++ show(c2 |*| c1 </> c1) 
+  print $ "c2 / c1: " ++ show(c2 </> c1) 
+  
+  
