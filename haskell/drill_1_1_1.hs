a0ba2f56d84882f4a1b62c65b00d96a298c05079
Balint Pato <balint.pato@balopat.com>
2016-11-26 23:35:40 -0500

    modulus + conj

--- haskell/drill_1_1_1.hs
+++ haskell/drill_1_1_1.hs
@@ -1,7 +1,7 @@
 import System.IO
 import Data.List.Split
 
-data Complex = Complex Float Float deriving (Show)
+data Complex = Complex Double Double deriving (Show)
 
 readComplex :: String -> Complex 
 readComplex s = let parts = splitOn " " s 
@@ -17,8 +17,14 @@ readComplex s = let parts = splitOn " " s
 (Complex r1 i1) |*| (Complex r2 i2) = Complex (r1 * r2 - i1 * i2) (r1 * i2 + r2 * i1)
 
 (</>) :: Complex -> Complex -> Complex
-(Complex r1 i1) </>  (Complex r2 i2) = Complex ((r1 * r2 + i1 * i2)/(r2*r2 + i2 * i2)) ((r2 * i1 - r1 * i2)/(r2*r2 + i2 * i2))
+(Complex r1 i1) </>  (Complex r2 i2) = Complex ((r1 * r2 + i1 * i2)/divisor) ((r2 * i1 - r1 * i2)/divisor)
+                                       where divisor = (r2*r2+ i2*i2)
 
+conj :: Complex -> Complex 
+conj (Complex r i) = Complex r (-i)
+
+modulus :: Complex -> Double 
+modulus (Complex r i) = sqrt $ r*r + i*i
 
 main = do
   print "Specify c1 (complex number) in form a b"   
@@ -27,8 +33,7 @@ main = do
   print "Specify c2 (complex number) in form a b"   
   c2String <- getLine
   
-  let c1 = readComplex c1String
-  
+  let c1 = readComplex c1String  
   let c2 = readComplex c2String 
   
   print $ "c1 + c2: " ++ show(c1 <+> c2)   
@@ -37,5 +42,9 @@ main = do
   print $ "c1 * c2: " ++ show(c1 |*| c2) 
   print $ "c2 * c1 / c1: " ++ show(c2 |*| c1 </> c1) 
   print $ "c2 / c1: " ++ show(c2 </> c1) 
+  print $ "modulus c1: " ++ show(modulus c1)
+  print $ "modulus c2: " ++ show(modulus c2)
+  print $ "conj c1: " ++ show(conj c1)
+  print $ "conj c2: " ++ show(conj c2)
   
   
