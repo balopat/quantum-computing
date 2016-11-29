30b5f4c3a33fb28659c04a883300453efd046f3b
Balint Pato <balint.pato@balopat.com>
2016-11-28 23:33:23 -0500

    drill 1.3.1 is  getting closer...have a processing js frontend talking to my happstack json backend, yay

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
-import Happstack.Server ( Browsing(EnableBrowsing), nullConf
-                        , serveDirectory, simpleHTTP
+import Control.Monad
+import Happstack.Server ( Method(GET,POST), method, Browsing(EnableBrowsing), nullConf
+                        , serveDirectory, simpleHTTP, serveFile, toResponse, asContentType, ok, dir, path, uriRest
                         )
+import Debug.Trace
+import Text.JSON.Generic
+import Complex
+
 
 main :: IO ()
-main = simpleHTTP nullConf $ serveDirectory EnableBrowsing [] "web"
+main = simpleHTTP nullConf $
+ msum [ dir "static" $ uriRest $ \s -> serveFile (asContentType "text/html") ("web/static/" ++ (trace ("value of s is: " ++ s) s) )
+        ,dir "complex" $ ok $ toResponse $ encodeJSON $ (Cartesian 1 1)
+        ,do method GET
+            serveFile (asContentType "text/html") "web/index.html"
+      ]
--- src/Complex.hs
+++ src/Complex.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Complex where
 
 import Data.List.Split
+import Text.JSON.Generic
 
-data Complex = Cartesian Double Double | Polar Double Double deriving (Show) 
+data Complex = Cartesian { r:: Double, i::Double } |  Polar { rho :: Double, theta::Double} deriving (Show,Data,Typeable)
 
-readComplex :: String -> Complex 
-readComplex s = let parts = splitOn " " s 
+readComplex :: String -> Complex
+readComplex s = let parts = splitOn " " s
           in Cartesian (read (head parts)) (read (parts!!1))
 
 (<+>) :: Complex -> Complex -> Complex
@@ -22,28 +24,25 @@ readComplex s = let parts = splitOn " " s
 (Cartesian r1 i1) </>  (Cartesian r2 i2) = Cartesian ((r1 * r2 + i1 * i2)/divisor) ((r2 * i1 - r1 * i2)/divisor)
                                        where divisor = r2*r2+i2*i2
 (Polar rho1 theta1) </>  (Polar rho2 theta2) = Polar (rho1/rho2) (theta1 -theta2)
-                                       
-conj :: Complex -> Complex 
+
+conj :: Complex -> Complex
 conj (Cartesian r i) = Cartesian r (-i)
 
-modulus :: Complex -> Double 
+modulus :: Complex -> Double
 modulus (Cartesian r i) = sqrt $ r*r + i*i
 
-toPolar :: Complex -> Complex 
+toPolar :: Complex -> Complex
 toPolar (Polar rho theta) = Polar rho theta
-toPolar (Cartesian r i)   = Polar rho theta 
+toPolar (Cartesian r i)   = Polar rho theta
                            where rho = modulus $ Cartesian r i
                                  theta = atan $ i/r
 
-toCartesian :: Complex -> Complex 
+toCartesian :: Complex -> Complex
 toCartesian (Polar rho theta) = Cartesian r i
                                 where r = rho * cos theta
-                                      i = rho * sin theta 
+                                      i = rho * sin theta
 toCartesian (Cartesian r i)   = Cartesian r i
 
 
-showPi :: Complex -> String 
-showPi (Polar rho theta) = show(rho) ++ ", " ++ show(theta/pi) ++ "pi" 
-
-  
-  
+showPi :: Complex -> String
+showPi (Polar rho theta) = show(rho) ++ ", " ++ show(theta/pi) ++ "pi"
