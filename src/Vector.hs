375c7ed0559a1faa3f0fdb800a4adbc3db8f6651
Balint Pato <balint.pato@balopat.com>
2016-12-07 08:51:50 -0500

    [drill 2.1.1] vector addition implemented

--- src/Vector.hs
+++ src/Vector.hs
@@ -1,1 +1,8 @@
 module Vector where
+import Complex
+
+add:: [Complex] -> [Complex] -> [Complex]
+add [] [] = []
+add a b = if length a /= length b then
+              error "Length are not equal"
+          else zipWith (<+>) a b
--- test/VectorSpec.hs
+++ test/VectorSpec.hs
@@ -1,13 +1,19 @@
 module VectorSpec(main, spec) where
 
 import Test.Hspec
-import Vector
+import qualified Vector as V
+import Complex
+import Control.Exception
 
 spec::Spec
 spec = do
   describe "Vector" $ do
     it "should add two vectors" $ do
-      1 `shouldBe` 1
+      (V.add [ Cartesian 1 2, Cartesian 2 3] [ Cartesian 1 1, Cartesian 2 2 ]) `shouldBe` [ Cartesian 2 3, Cartesian 4 5 ]
+    it "sum of empty vectors is empty vector" $ do
+        (V.add [] []) `shouldBe` []
+    it "sum of  vectors with different sizes results in errorr" $ do
+        evaluate(V.add [] [Cartesian 1 1]) `shouldThrow` errorCall "Length are not equal"
 
 main::IO()
 main = do
