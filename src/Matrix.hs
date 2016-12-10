16979b0b73d9d976476806a976582adf6a107288
Balint Pato <balint.pato@balopat.com>
2016-12-10 18:51:25 -0500

    serious refactor for Matrix multiplication - I'm saving the dimensions upfront into the M.Matrix type and validate it with a smart constructor M.matrix :: [[Complex]] -> Matrix

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -1,17 +1,34 @@
-module Matrix where
+module Matrix (matrix, add, inv, scalar, Matrix, mul) where
 import Complex
 import qualified Vector as V
+import  Control.Exception
 
-add:: [[Complex]] -> [[Complex]] -> [[Complex]]
-add [] [] = []
-add a b = if length a /= length b then
-              error "Sizes are not equal"
-          else zipWith (V.add) a b
+data Matrix = Cnm {
+    mx :: [[Complex]],
+    n :: Int,
+    m :: Int
+} deriving (Show, Eq)
 
-inv:: [[Complex]] -> [[Complex]]
-inv [] = []
-inv rows = [[ Cartesian (-a) (-b) | (Cartesian a b) <- row] | row <- rows]
+matrix :: [[Complex]] -> Matrix
+matrix [] = Cnm [] 0 0
+matrix rows = let n = length rows
+                  m = length (head rows) in
+              assert (all (\row -> length row == m) rows) (Cnm rows n m)
 
-scalar:: Complex -> [[Complex]] -> [[Complex]]
-scalar _ [] = []
-scalar s rows = [[ s |*| x | x <- row] | row <- rows]
+
+add:: Matrix -> Matrix -> Matrix
+add (Cnm [] 0 0) (Cnm [] 0 0) = Cnm [] 0 0
+add (Cnm a n1 m1) (Cnm b n2 m2) = if (n1 /= n2) || (m1 /= m2) then
+            error ("Sizes are not equal: " ++ (show n1) ++ "x" ++ (show m1) ++ " vs "  ++ (show n2) ++ "x" ++ (show m2))
+          else Cnm (zipWith (V.add) a b) n1 m1
+
+inv:: Matrix -> Matrix
+inv (Cnm [] 0 0) = Cnm [] 0 0
+inv (Cnm rows _ _) = matrix [[ Cartesian (-a) (-b) | (Cartesian a b) <- row] | row <- rows]
+
+scalar:: Complex -> Matrix -> Matrix
+scalar _ (Cnm [] 0 0) = (Cnm [] 0 0)
+scalar s (Cnm rows _ _) = matrix [[ s |*| x | x <- row] | row <- rows]
+
+mul:: Matrix -> Matrix -> Matrix
+mul (Cnm [] 0 0) (Cnm [] 0 0) = (Cnm [] 0 0)
--- test/MatrixSpec.hs
+++ test/MatrixSpec.hs
@@ -10,26 +10,30 @@ spec = do
   describe "Complex Matrix operations:" $ do
     describe "Matrix.add:" $ do
       it "should add two matrices element by element" $ do
-        let m1 = [[ Cartesian 1 2, Cartesian 2 3]
-                  ,[ Cartesian 1 2, Cartesian 2 3]]
-            m2 = [[ Cartesian 1 1, Cartesian 2 2 ],
-                  [ Cartesian 1 1, Cartesian 2 2 ]] 
-        (M.add m1 m2) `shouldBe` [[ Cartesian 2 3, Cartesian 4 5 ],
-                                  [ Cartesian 2 3, Cartesian 4 5 ]]
+        let m1 = (M.matrix [[ Cartesian 1 2, Cartesian 2 3]
+                  ,[ Cartesian 1 2, Cartesian 2 3]])
+            m2 = (M.matrix [[ Cartesian 1 1, Cartesian 2 2 ],
+                  [ Cartesian 1 1, Cartesian 2 2 ]])
+            expected = (M.matrix [[ Cartesian 2 3, Cartesian 4 5 ],
+                                      [ Cartesian 2 3, Cartesian 4 5 ]])
+        (M.add m1 m2) `shouldBe` expected
       it "sum of empty matrices is empty matrix" $ do
-          (M.add [] []) `shouldBe` []
+          (M.add (M.matrix [])  (M.matrix []) ) `shouldBe` (M.matrix [])
       it "sum of matrices with different sizes results in error" $ do
-          evaluate(M.add [] [[Cartesian 1 1]]) `shouldThrow` errorCall "Sizes are not equal"
+          evaluate(M.add (M.matrix []) (M.matrix [[Cartesian 1 1]])) `shouldThrow` errorCall "Sizes are not equal: 0x0 vs 1x1"
     describe "Matrix.inv (inverse):" $ do
       it "should negate a matrix's elements" $ do
-        (M.inv [[ Cartesian 1 2, Cartesian (-2) 3]]) `shouldBe` [[ Cartesian (-1) (-2), Cartesian 2 (-3)]]
+        (M.inv (M.matrix [[ Cartesian 1 2, Cartesian (-2) 3]])) `shouldBe` M.matrix [[ Cartesian (-1) (-2), Cartesian 2 (-3)]]
       it "inverse of empty matrix is empty matrix" $ do
-          (M.inv []) `shouldBe` []
+          M.inv (M.matrix []) `shouldBe` M.matrix []
     describe "Matrix.scalar:" $ do
       it "should multiply a matrix's elements" $ do
-        (M.scalar (Cartesian (-3) 0) [[ Cartesian 1 2, Cartesian (-2) 3]]) `shouldBe` [[ Cartesian (-3) (-6), Cartesian 6 (-9)]]
-      it "product of any scalar with empty vector is empty vector" $ do
-          (M.scalar (Cartesian 100 1) []) `shouldBe` []
+        (M.scalar (Cartesian (-3) 0) (M.matrix [[ Cartesian 1 2, Cartesian (-2) 3]])) `shouldBe` (M.matrix [[ Cartesian (-3) (-6), Cartesian 6 (-9)]])
+      it "product of any scalar with empty vector is empty matrix" $ do
+          M.scalar  (Cartesian 100 1) (M.matrix []) `shouldBe` M.matrix []
+    describe "Matrix.mul:" $ do
+      it "the product of empty and empty is empty" $ do
+        M.mul (M.matrix [])  (M.matrix []) `shouldBe` (M.matrix [])
 
 
 main::IO()
